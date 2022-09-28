76283c1cb0cb95cb41ce5285053b4b6a5527f3f4
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-09-27 22:02:16 -0500

    completo hasta front-end logica numeral 3

---
+++ src/main/resources/static/js/apimock.js
@@ -0,0 +1,44 @@
+//@author hcadavid
+
+apimock=(function(){
+
+	var mockdata=[];
+
+	mockdata["johnconnor"]=	[
+	{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
+	{author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"},
+	{author:"johnconnor","points":[{"x":360,"y":220},{"x":10,"y":210}],"name":"duty"},
+	{author:"johnconnor","points":[{"x":370,"y":210},{"x":152,"y":212}],"name":"room"}];
+
+	mockdata["maryweyland"]=[
+	{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
+	{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"},
+	{author:"maryweyland","points":[{"x":120,"y":120},{"x":115,"y":125}],"name":"duty2"},
+	{author:"maryweyland","points":[{"x":130,"y":111},{"x":125,"y":135}],"name":"room2"}];
+
+
+	return {
+		getBlueprintsByAuthor:function(authname,callback){
+			callback(
+				mockdata[authname]
+			);
+		},
+
+		getBlueprintsByNameAndAuthor:function(authname,bpname,callback){
+
+			callback(
+				mockdata[authname].find(function(e){return e.name===bpname})
+			);
+		}
+	}	
+
+})();
+
+/*
+Example of use:
+var fun=function(list){
+	console.info(list);
+}
+
+apimock.getBlueprintsByAuthor("johnconnor",fun);
+apimock.getBlueprintsByNameAndAuthor("johnconnor","house",fun);*/
